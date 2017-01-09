20f040ec9ec71cf4ea0306ed05663c3700ff8428
bowsil <bowsil@BOWSIL-PC>
2017-01-09 10:47:29 +0400

    First Time update

---
+++ UrduAudio.Web/Demo.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UrduAudio.Log;
+
+namespace UrduAudio.Web
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UrduAudio.Log.ErrLog.LogDefaultError("This is for testing");
+
+            //Declare an instance for log4net
+
+        }
+    }
+}
