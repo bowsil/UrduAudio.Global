e4234fe54ecca699716e856cf2ab76aa93a3c6c7
bowsil <bowsil@BOWSIL-PC>
2017-01-09 11:49:01 +0400

    Demo testing

---
+++ UrduAudio.Log/ErrLog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace UrduAudio.Log
+{
+    public class ErrLog
+    {
+
+        private static readonly ILog Log =
+                LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void LogDefaultError(string errorString)
+        {
+
+            Log.Fatal(errorString);
+
+
+        }
+        public static void LogWarning(string errorString)
+        {
+
+
+            Log.Warn(errorString);
+
+
+        }
+
+        public static void LogInfo(string errorString)
+        {
+
+
+            Log.Info(errorString);
+
+
+        }
+
+        public static void LogDebug(string errorString)
+        {
+
+
+            Log.Debug(errorString);
+
+
+        }
+
+    }
+
+}
